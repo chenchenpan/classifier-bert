057325afaf98aceb24ddab341a4a8322344b105d
Ubuntu <myvm@myvm.qt11odus5ktu1avruz5azmyzgb.xx.internal.cloudapp.net>
2020-07-04 08:12:31 +0000

    change file.

--- bert/kickstarter_classifier_train_large.sh
+++ bert/kickstarter_classifier_train_large.sh
@@ -14,7 +14,7 @@ python run_classifier.py \
   --bert_config_file=$BERT_BASE_DIR/bert_config.json \
   --init_checkpoint=$BERT_BASE_DIR/bert_model.ckpt \
   --max_seq_length=128 \
-  --train_batch_size=16 \
+  --train_batch_size=6 \
   --learning_rate=3e-5 \
   --num_train_epochs=3.0 \
   --output_dir=$TMP/kick_output_large/
